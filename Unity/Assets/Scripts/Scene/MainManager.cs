0c6ddf763086334470f04399fdc247c1d84e7d8e
DESKTOP-P45UENC\sigum <sigumax.kyorosuke@outlook.jp>
2017-12-11 18:30:28 +0900

    no message

---
+++ Unity/Assets/Scripts/Scene/MainManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        SceneUpdate();
+    }
+
+    private void SceneUpdate()
+    {
+    }
+
+    private void SceneInit()
+    {
+    }
+
+    private void SceneClear()
+    {
+    }
+
+    private void SceneOver()
+    {
+    }
+}
---
+++ Unity/Assets/Scripts/Scene/TitleManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThisManager : MonoBehaviour
+{
+
+	private void Start()
+	{
+	}
+
+	private void Update()
+	{
+		SceneUpdate();
+	}
+
+	private void SceneUpdate()
+	{
+	}
+
+	private void SceneInit()
+	{
+	}
+}
