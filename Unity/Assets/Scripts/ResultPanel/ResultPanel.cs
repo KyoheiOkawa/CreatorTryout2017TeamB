b3d5372395783d0431e656757c1a1f3dc4c97b93
KyoheiOkawa <kurokenken0310@gmail.com>
2017-12-13 11:01:49 +0900

    時間を待ってからフェードを始められるようにしました

--- Unity/Assets/Scripts/ResultPanel/ResultPanel.cs
+++ Unity/Assets/Scripts/ResultPanel/ResultPanel.cs
@@ -13,13 +13,13 @@ public class ResultPanel : MonoBehaviour
 		Scene scene;
 		scene = SceneManager.GetActiveScene ();
 
-		FadeManager.Instance.Transition (0.5f, scene.name);
+		FadeManager.Instance.Transition (0.5f, scene.name,0.2f);
         GameObject.Find("AudioManager").GetComponent<AudioManager>().PlaySE("enter",0.1f);
     }
 
 	public void OnTitleButton()
 	{
-		FadeManager.Instance.Transition(0.5f,titleSceneName);
+		FadeManager.Instance.Transition(0.5f,titleSceneName,0.2f);
         GameObject.Find("AudioManager").GetComponent<AudioManager>().PlaySE("enter", 0.1f);
     }
 }
