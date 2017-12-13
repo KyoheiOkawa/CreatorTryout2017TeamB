70b9cce387aa3798c4002604bbafa66a92ce3a1b
kanekoseiya <seiya_555_0701@yahoo.co.jp>
2017-12-13 16:36:45 +0900

    [カメラの処理の修正] [エネミーのスクリプトの修正] [プレイヤーコントロールの修正]

--- Unity/Assets/Scripts/CameraScript.cs
+++ Unity/Assets/Scripts/CameraScript.cs
@@ -4,21 +4,8 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
-	[SerializeField]
-	private GameObject player;
-
-	void Start () 
-	{
-		
-	}
-
 	void Update () 
 	{
-		transform.position = player.transform.position - new Vector3 (-4, 0, 10);
-	}
-
-	public void SetTargetObject(GameObject target)
-	{
-		player = target;
-	}
+        transform.position = PlayerController.Instance.transform.position - new Vector3(-4, 0, 10);
+    }
 }
