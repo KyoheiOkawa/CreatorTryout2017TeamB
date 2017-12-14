10f28b027f3f50dccdf0cf255642e8c6a78afd8e
KyoheiOkawa <kurokenken0310@gmail.com>
2017-12-14 20:56:32 +0900

    リザルトパネルフェード

--- Unity/Assets/Scripts/ResultPanel/ResultPanel.cs
+++ Unity/Assets/Scripts/ResultPanel/ResultPanel.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ResultPanel : MonoBehaviour 
 {
 	[SerializeField]
 	string titleSceneName;
+
+	[SerializeField]
+	Image panelImg;
+
+	[SerializeField]
+	Button retryButton;
+
+	[SerializeField]
+	Button titleButton;
+
+	[SerializeField]
+	float fadeTime = 0.5f;
+
+	void Start()
+	{
+		retryButton.enabled = false;
+		retryButton.enabled = false;
+		panelImg.color = new Color(1,1,1,0);
+
+		StartCoroutine (PanelFade ());
+	}
 
 	public void OnRetryButton()
 	{
@@ -24,4 +46,25 @@ public class ResultPanel : MonoBehaviour
 		FadeManager.Instance.SetRayCastBlock (false);
         GameObject.Find("AudioManager").GetComponent<AudioManager>().PlaySE("enter", 0.1f);
     }
+
+	IEnumerator PanelFade()
+	{
+		while (true)
+		{
+			Color color = panelImg.color;
+			color.a += Time.deltaTime * fadeTime;
+			panelImg.color = color;
+
+			if (panelImg.color.a >= 1.0f) 
+			{
+				panelImg.color = new Color(1,1,1,1);
+				break;
+			}
+
+			yield return null;
+		}
+
+		retryButton.enabled = true;
+		retryButton.enabled = true;
+	}
 }
